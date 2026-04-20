9643065c7d33e2c976eabf293c6dee77acc088c2
JCHACKER2024 <jorge_1616_costa@hotmail.com>
2026-04-20 20:21:20 +0100

    Add files via upload

---
+++ Site CAM/script.js
@@ -0,0 +1,11 @@
+// Exemplo: Scroll suave para as secções
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+console.log("Site de CAM carregado com sucesso!");
